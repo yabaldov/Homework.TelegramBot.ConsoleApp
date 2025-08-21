0798b15cab2c8aa8baf9e87ffd5ea8f799b7268d
Alexey Baldov <alexey-baldov@yandex.ru>
2025-08-21 21:14:58 +0400

    feat: добавляет выполнение пункта 1 из ДЗ № 4
    Добавить глобальный try catch

--- Homework.TelegramBot.ConsoleApp/Program.cs
+++ Homework.TelegramBot.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using Homework.TelegramBot.ConsoleApp;
+﻿using System;
+using System.IO;
+using Homework.TelegramBot.ConsoleApp;
 
 namespace Homework.TelegramBot.ConsoleApp
 {
@@ -6,8 +8,37 @@ namespace Homework.TelegramBot.ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			Bot bot = new Bot();
-			bot.Run();
+			bool hasError = false;
+			try
+			{
+				var bot = new Bot();
+				bot.Run();
+
+			}
+			catch (Exception ex)
+			{
+				hasError = true;
+				Console.WriteLine($"Произошла непредвиденная ошибка: {ex.GetType().Name}");
+				Console.WriteLine($"Описание ошибки: {ex.Message}");
+				Console.WriteLine($"Трассировка стека:{Environment.NewLine}{ex.StackTrace}");
+				if (ex.InnerException != null)
+				{
+					Console.WriteLine($"Внутреннее исключение:{Environment.NewLine}{ex.InnerException.Message}");
+				}
+
+			}
+			finally
+			{
+				if (hasError)
+				{
+					Console.WriteLine("Программа завершена с ошибкой. Нажмите любую клавишу для выхода.");
+				}
+				else
+				{
+					Console.WriteLine("Программа завершена успешно. Нажмите любую клавишу для выхода.");
+				}
+				Console.ReadKey(true);
+			}	
 		}
 	}
 }
