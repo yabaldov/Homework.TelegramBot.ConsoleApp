aff050ae08892cd8cd7c81fce929fff1d16d599d
Alexey Baldov <alexey-baldov@yandex.ru>
2026-01-20 15:41:14 +0400

    feat: Д3 п. 3. Изменяет класс ToDoUser.

--- Homework.TelegramBot.ConsoleApp/ToDoUser.cs
+++ Homework.TelegramBot.ConsoleApp/ToDoUser.cs
@@ -5,12 +5,14 @@ namespace Homework.TelegramBot.ConsoleApp
     public class ToDoUser
     {
         public Guid UserId { get; }
+        public long TelegramUserId { get; }
         public string TelegramUserName { get; }
         public DateTime RegisteredAt { get; }
 
-        public ToDoUser(string telegramUserName)
+        public ToDoUser(long telegramUserId, string telegramUserName)
         {
             UserId = Guid.NewGuid();
+            TelegramUserId = telegramUserId;
             TelegramUserName = telegramUserName;
             RegisteredAt = DateTime.UtcNow;
         }
